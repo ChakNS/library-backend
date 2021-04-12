d921638b3be59c8f579950969fe3c2407501dde8
Chak <chakcheung4565@gmail.com>
2021-04-12 11:54:53 +0800

    cors init

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser'); //这就是一个解析Cookie的工具。通过req.cookies可以取到传过来的cookie，并把它们转成对象。
 var logger = require('morgan'); //在控制台中，显示req请求的信息
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 //配置路由模块
 app.use('/', indexRouter);
