4a6f937eded790f3fb58f1730701bbe61358db51
zekaiz <chakcheung4565@outlook.com>
2021-07-22 17:59:03 +0800

    nothing

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,9 @@ const RouterHandle = require('../utils/router-handler');
 const router = new RouterHandle()
 
 router.$post('/', async function({ body }, res, next) {
+  console.log(111)
+  console.log(process.env)
+  console.log(mysql)
   mysql('SELECT * FROM menus').then((result) => {
     res.status(200).send(result);
   })
